2dfad157857e5ab3dede2f49434260684743d31d
Bruno Mazzocchi <bemazzocchi@gmail.com>
2022-03-22 15:11:26 -0600

    Created PurchaseRepostory

--- src/main/java/com/example/market/domain/repository/PurchaseRepository.java
+++ src/main/java/com/example/market/domain/repository/PurchaseRepository.java
@@ -1,4 +1,11 @@
 package com.example.market.domain.repository;
 
+import com.example.market.domain.*;
+
+import java.util.*;
+
 public interface PurchaseRepository {
+    List<Purchase> getAll(); //Muestra las compras
+    Optional<List<Purchase>> getByCliente(String clientId); //Muestra quien hizo la compra por el ID del cliente
+    Purchase save(Purchase purchase); //Guarda una compra
 }
