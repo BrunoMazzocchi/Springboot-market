cc9febf6180067891c9b8f9709df30547deb42b7
Bruno Mazzocchi <bemazzocchi@gmail.com>
2022-03-24 11:00:47 -0600

    Documentation with Swagger

---
+++ src/main/java/com/example/market/web/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.example.market.web.config;
+
+
+import org.springframework.context.annotation.*;
+import springfox.documentation.builders.*;
+import springfox.documentation.spi.*;
+import springfox.documentation.spring.web.plugins.*;
+import springfox.documentation.swagger2.annotations.*;
+
+@Configuration //Para habilitar swagger
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean //
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("package com.example.market.web.controller"))
+                .build();
+    }
+}
