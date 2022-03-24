4e72adc6af3cb9b0ec25b35baa7a15c753bd4dd7
Bruno Mazzocchi <bemazzocchi@gmail.com>
2022-03-24 17:42:34 -0600

    Working on JWT generator

---
+++ src/main/java/com/example/market/web/secuirty/JWTUtil.java
@@ -0,0 +1,17 @@
+package com.example.market.web.secuirty;
+
+import io.jsonwebtoken.*;
+import org.springframework.security.core.userdetails.*;
+
+import java.util.*;
+
+public class JWTUtil {
+    private static final String KEY = "testing";
+
+    public String generateToken(UserDetails userDetails){
+        //Genera un JWT y lo retorna
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10 ))
+                .signWith(SignatureAlgorithm.HS256, KEY).compact();
+    }
+}
